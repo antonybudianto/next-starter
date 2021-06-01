83595abc8e71b19b3631e3b1c333b78abf1556d3
Antony <antony.budianto@tokopedia.com>
2021-06-01 15:07:07 +0700

    add eslint

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,14 @@ export default function Home() {
       </Head>
 
       <div className="px-4 sm:px-6 md:px-8">
-        <div className="text-center">
+        <div className="">
           <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 mt-8 mb-8 sm:mt-14 sm:mb-10">
             Welcome
           </h1>
           <p className="text-gray-500 text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">
             Starter for any use case
           </p>
-          <div className="flex flex-wrap justify-center space-y-4 sm:space-y-0 sm:space-x-4 text-center">
+          <div className="flex flex-wrap justify-center space-y-4 sm:space-y-0 sm:space-x-4 ">
             <a
               className="w-full sm:w-auto flex-none bg-gray-900 hover:bg-gray-700 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
               href="/docs"
